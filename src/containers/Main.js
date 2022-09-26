8425d383011d97f902dbbbb6fa0b78166199256d
Areeb Khan <areebkhan123123@gmail.com>
2022-09-26 18:33:25 +0500

    changes

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -17,6 +17,7 @@ export default function Main(propss) {
             <Route
               path="/"
               exact
+              
               render={(props) => (
                 <Splash
                   {...props}
